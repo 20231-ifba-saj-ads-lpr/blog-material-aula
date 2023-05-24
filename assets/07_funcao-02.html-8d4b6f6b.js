50f90777d0db9785bd63d02ee1cafab96a09901b
leandro-costa <leandro.costa@ifba.edu.br>
2023-05-24 22:03:30 +0000

    Deploying to gh-pages from @ 20231-ifba-saj-ads-lpr/blog-material-aula@3b481f96c421e99f054bc313f6a68886a0d9ad08 🚀

--- assets/07_funcao-02.html-8d4b6f6b.js
+++
@@ -1,8 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as a,c as n,e as i}from"./app-a2866b62.js";const o={},r=i(`<h1 id="exercicio-06-funcao-02" tabindex="-1"><a class="header-anchor" href="#exercicio-06-funcao-02" aria-hidden="true">#</a> Exercício 06 Funcão 02</h1><ol><li>Elabore uma função que receba como parâmetro um valor inteiro n e gere como saída um triângulo lateral formado por asteriscos conforme o exemplo a seguir, em que usamos n = 4.</li></ol><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>*
-**
-***
-****
-***
-**
-*
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,3),s=[r];function c(l,d){return a(),n("div",null,s)}const m=e(o,[["render",c],["__file","07_funcao-02.html.vue"]]);export{m as default};
